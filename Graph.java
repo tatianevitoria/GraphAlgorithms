cd034ac92d69cac704d23d5113f493ea261510c1
Tatiane Vitória de Oliveira <93017237+tatianevitoria@users.noreply.github.com>
2022-08-30 02:26:47 +0000

    Atividade Prática III

--- Graph.java
+++ Graph.java
@@ -1,86 +1,227 @@
-class Graph{
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+class Graph {
   private int countNodes;
   private int countEdges;
   private int[][] adjMatrix;
 
-  public Graph(int countNodes){
+  public Graph(int countNodes) {
     this.countNodes = countNodes;
-    this.adjMatrix = new int [countNodes][countNodes]; 
+    this.adjMatrix = new int[countNodes][countNodes];
   }
 
-  public int getCountNodes(){
+  public int getCountNodes() {
     return this.countNodes;
   }
 
-  public int getCountEdges(){
+  public int getCountEdges() {
     return this.countEdges;
   }
 
-  public int[][] getAdjMatrix(){
+  public int[][] getAdjMatrix() {
     return this.adjMatrix;
   }
 
-  public String toString(){
+  public String toString() {
     String str = "";
-    for(int i = 0; i < this.adjMatrix.length; i++){
-      for(int j = 0; j < this.adjMatrix[i].length; j++){
+    for (int i = 0; i < this.adjMatrix.length; i++) {
+      for (int j = 0; j < this.adjMatrix[i].length; j++) {
         str += this.adjMatrix[i][j] + "\t";
       }
       str += "\n";
     }
     return str;
   }
-  
-  public void addEdge(int source, int sink, int weight){
-    if(source < 0 || source > this.countNodes -1|| sink <0 || sink > this.countNodes -1|| weight <= 0){
-        System.err.println("Invalid value for source, sink or weight!");
+
+  public void addEdge(int source, int sink, int weight) {
+    if (source < 0 || source > this.countNodes - 1 || sink < 0 || sink > this.countNodes - 1 || weight <= 0) {
+      System.err.println("Invalid value for source, sink or weight!");
       return;
     }
-    this.adjMatrix[source][sink] = weight;  
+    this.adjMatrix[source][sink] = weight;
     this.countEdges++;
   }
 
-  public int degree(int node){
+  public void addEdgeUnoriented(int u, int v, int w) {
+    if (u < 0 || u > this.countNodes - 1 || v < 0 || v > this.countNodes - 1 || w <= 0) {
+      System.err.println("Invalid value for source, sink or weight!");
+      return;
+    }
+    this.adjMatrix[u][v] = w;
+    this.adjMatrix[v][u] = w;
+    this.countEdges += 2;
+  }
+
+  public int degree(int node) {
     int degree = 0;
-    for(int i = 0; i < adjMatrix[node].length; i++){
-      if(this.adjMatrix[node][i] != 0){
+    for (int i = 0; i < adjMatrix[node].length; i++) {
+      if (this.adjMatrix[node][i] != 0) {
         degree++;
       }
     }
     return degree;
   }
 
-  public int highestDegree(){
+  public int highestDegree() {
     int count = 0;
-    for(int i = 0; i < this.adjMatrix.length; i++){ 
+    for (int i = 0; i < this.adjMatrix.length; i++) {
       int aux = degree(i);
-      if(aux > count){
+      if (aux > count) {
         count = aux;
       }
     }
     return count;
   }
 
-  public int lowestDegree(){
+  public int lowestDegree() {
     int count = 0;
-    for(int i = 0; i < this.adjMatrix.length; i++){ 
+    for (int i = 0; i < this.adjMatrix.length; i++) {
       int aux = degree(i);
-      if(aux <= count){
+      if (aux <= count) {
         count = aux;
       }
     }
-      return count;
-    }
-  
-  public Graph complement(){
-    Graph g2 =  new Graph(this.adjMatrix.length);
-    for(int i = 0; i < this.adjMatrix.length; i++){
-      for(int j = 0; j < this.adjMatrix[i].length; j++){
-        if(i != j && this.adjMatrix[i][j] == 0){
+    return count;
+  }
+
+  public Graph complement() {
+    Graph g2 = new Graph(this.adjMatrix.length);
+    for (int i = 0; i < this.adjMatrix.length; i++) {
+      for (int j = 0; j < this.adjMatrix[i].length; j++) {
+        if (i != j && this.adjMatrix[i][j] == 0) {
           g2.addEdge(i, j, 1);
         }
       }
     }
     return g2;
   }
+
+  public float density() {
+    return (float) (this.countEdges * 2) / (this.countNodes * (this.countNodes - 1));
+  }
+
+  public boolean subGraph(Graph g2) {
+    if (g2.countNodes > this.countNodes || g2.countEdges > this.countEdges)
+      return false;
+    for (int i = 0; i < g2.adjMatrix.length; i++) {
+      for (int j = 0; j < g2.adjMatrix[i].length; j++) {
+        if (this.adjMatrix[i][j] == 0 && g2.adjMatrix[i][j] != 0)
+          return false;
+      }
+    }
+    return true;
+  }
+
+  public ArrayList<Integer> bfs(int s) {
+
+    int[] desc = new int[this.countNodes];
+    ArrayList<Integer> Q = new ArrayList<>();
+    Q.add(s);
+    ArrayList<Integer> R = new ArrayList<>();
+    R.add(s);
+    desc[s] = 1;
+
+    while (Q.size() > 0) {
+      int u = Q.remove(0);
+      for (int v = 0; v < this.adjMatrix[u].length; v++) {
+        if (this.adjMatrix[u][v] != 0) {
+          if (desc[v] == 0) {
+            Q.add(v);
+            R.add(v);
+            desc[v] = 1;
+          }
+        }
+      }
+    }
+    return R;
+  }
+
+  public boolean connected() {
+    return this.bfs(0).size() == this.countNodes;
+  }
+
+  /*
+   * public Graph(String fileName) throws IOException {
+   * File file = new File(fileName);
+   * FileReader reader = new FileReader(file);
+   * BufferedReader bufferedReader = new BufferedReader(reader);
+   * 
+   * // Read header
+   * String[] line = bufferedReader.readLine().split(" ");
+   * this.countNodes = (Integer.parseInt(line[0]));
+   * int fileLines = (Integer.parseInt(line[1]));
+   * 
+   * // Create and fill adjMatrix with read edges
+   * this.adjMatrix = new int[this.countNodes][this.countNodes];
+   * for (int i = 0; i < fileLines; ++i) {
+   * String[] edgeInfo = bufferedReader.readLine().split(" ");
+   * int source = Integer.parseInt(edgeInfo[0]);
+   * int sink = Integer.parseInt(edgeInfo[1]);
+   * int weight = Integer.parseInt(edgeInfo[2]);
+   * addEdge(source, sink, weight);
+   * }
+   * bufferedReader.close();
+   * reader.close();
+   * }
+   */
+
+  public ArrayList<Integer> dfs(int s) {
+    int[] desc = new int[this.countNodes];
+    ArrayList<Integer> S = new ArrayList<>();
+    S.add(s);
+    ArrayList<Integer> R = new ArrayList<>();
+    R.add(s);
+    desc[s] = 1;
+
+    while (S.size() > 0) {
+      int u = S.get(S.size() - 1);
+      boolean unstack = true;
+      for (int v = 0; v < this.adjMatrix[u].length; v++) {
+        if (this.adjMatrix[u][v] != 0 && desc[v] == 0) {
+          S.add(v);
+          R.add(v);
+          desc[v] = 1;
+          unstack = false;
+          break;
+        }
+      }
+      if (unstack) {
+        S.remove(S.size() - 1);
+      }
+    }
+
+    return R;
+  }
+
+  public boolean nonOriented(){
+    for(int i = 0; i < this.adjMatrix.length; i++){
+      for(int j = 0; j < this.adjMatrix[i].length; j++){//j = i+1 reduz o tempo
+        if(this.adjMatrix[i][j] != this.adjMatrix[j][i]){
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  public ArrayList<Integer> dfs_rec(int s){
+    int[] desc = new int[this.countNodes];
+    ArrayList<Integer> R = new ArrayList<>();
+    dfs_recAux(s, desc, R);
+    return R;
+  }
+  
+  public void dfs_recAux(int u, int[] desc, ArrayList<Integer> R) {
+    desc[u] = 1;
+    R.add(u);
+    for (int v = 0; v < this.adjMatrix[u].length; ++v) {
+      if (this.adjMatrix[u][v] != 0 && desc[v] == 0) {
+        dfs_recAux(v, desc, R);
+      }
+    }
+  }
+    
 }
--- Main.java
+++ Main.java
@@ -1,6 +1,33 @@
+import java.io.IOException;
+
 class Main {
-  public static void main(String[] args) {
-    Graph g1 = new Graph(4);
+  public static void main(String[] args) throws IOException {
+
+  Graph g1 = new Graph(9);
+    g1.addEdgeUnoriented(7, 5, 1);
+    g1.addEdgeUnoriented(7, 1, 1);
+    g1.addEdgeUnoriented(7, 2, 1);
+    g1.addEdgeUnoriented(1, 0, 1);
+    g1.addEdgeUnoriented(1, 4, 1);
+    g1.addEdgeUnoriented(2, 3, 1);
+    g1.addEdgeUnoriented(5, 6, 1);
+    g1.addEdgeUnoriented(6, 8, 1);
+    System.out.println(g1.bfs(7));
+    System.out.println(g1.connected());
+    System.out.println(g1.dfs_rec(5));
+
+    Graph g2 = new Graph(4);
+    g2.addEdge(0, 1, 1);
+    g2.addEdge(0, 3, 1);
+    g2.addEdge(1, 0, 1);
+    g2.addEdge(2, 1, 1);
+    g2.addEdge(3, 1, 1);
+    System.out.println(g2.nonOriented());
+
+    //Graph g2 = new Graph("graph1.txt");
+    //System.out.println(g2);
+    
+   /*  Gbusca_Largura(s)raph g1 = new Graph(4);
     g1.addEdge(0, 1 , 3);
     g1.addEdge(1, 0 , 3);
     g1.addEdge(0, 3 , 4);
@@ -11,6 +38,15 @@ class Main {
     System.out.println(g1.degree(1));
     System.out.println("Maior grau: " + g1.highestDegree());
     System.out.println("Menor grau: " + g1.lowestDegree());
-    System.out.println("\n" + g1.complement().toString());
+    System.out.println("\n" + "Complemento: " +"\n"+ g1.complement().toString());
+    System.out.println("Densidade: " + g1.density());
+    Graph g2 = new Graph(3);
+    g2.addEdge(0, 1, 1);
+    g2.addEdge(1, 0, 1);
+    System.out.println("g2 é subGrafo? " + g1.subGraph(g2)); 
+    Graph g3 = new Graph(4);
+    g3.addEdge(1, 0, 1);
+    g3.addEdge(3, 1, 1);
+    System.out.println("g3 é subGrafo? " + g1.subGraph(g3)); */
   }
 }
